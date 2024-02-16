063fd4bc989c3a975d6a7aa913084e4719f71005
rofore <robin.reutimann@gmail.com>
2024-02-16 11:01:02 +0100

    Improved formatting

--- epan/dissectors/packet-ltoudp.c
+++ epan/dissectors/packet-ltoudp.c
@@ -1,8 +1,13 @@
-// from https://www.wireshark.org/docs/wsdg_html_chunked/ChDissectAdd.html
+/* Basic LocalTalk over UDP (LToUDP) dissector */
+/* For more informations about LToUDP, see
+ * https://windswept.home.blog/2019/12/10/localtalk-over-udp/ */
+/* This dissector is based upon
+ * https://www.wireshark.org/docs/wsdg_html_chunked/ChDissectAdd.html */
+
+#include <epan/etypes.h>
+#include <epan/packet.h>
 
 #include "config.h"
-#include <epan/packet.h>
-#include <epan/etypes.h>
 
 #define LTOUDP_PORT 1954
 
@@ -12,11 +17,12 @@ static int hf_sender_id = -1;
 static gint ett_foo = -1;
 static dissector_handle_t llap_handle;
 
-
-static int
-dissect_ltoudp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
+static int dissect_ltoudp(tvbuff_t *tvb,
+                          packet_info *pinfo,
+                          proto_tree *tree _U_,
+                          void *data _U_)
 {
-    tvbuff_t	*next_tvb;
+    tvbuff_t *next_tvb;
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "ltoudp");
     /* Clear the info column */
     col_clear(pinfo->cinfo, COL_INFO);
@@ -25,9 +31,8 @@ dissect_ltoudp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *da
     proto_tree *ltoudp_tree = proto_item_add_subtree(ti, ett_foo);
     proto_tree_add_item(ltoudp_tree, hf_sender_id, tvb, 0, 4, ENC_BIG_ENDIAN);
 
-	next_tvb = tvb_new_subset_remaining(tvb, 4);
-	call_dissector(llap_handle, next_tvb, pinfo, tree);
-
+    next_tvb = tvb_new_subset_remaining(tvb, 4);
+    call_dissector(llap_handle, next_tvb, pinfo, tree);
 
     return tvb_captured_length(tvb);
 }
@@ -35,20 +40,28 @@ dissect_ltoudp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *da
 void proto_register_ltoudp(void)
 {
     static hf_register_info hf[] = {
-        {&hf_sender_id,
-         {"Sender ID", "ltoudp.senderId",
-          FT_UINT32, BASE_HEX,
-          NULL, 0x0,
-          NULL, HFILL}}};
+        {
+            &hf_sender_id,
+            {
+                "Sender ID",
+                "ltoudp.senderId",
+                FT_UINT32,
+                BASE_HEX,
+                NULL,
+                0x00,
+                NULL,
+                HFILL
+            }
+        }
+    };
 
     /* Setup protocol subtree array */
-    static gint *ett[] = {
-        &ett_foo};
+    static gint *ett[] = {&ett_foo};
 
     proto_ltoudp = proto_register_protocol(
-        "LocalTalk over UDP", /* name        */
-        "LToUDP",             /* short_name  */
-        "ltoudp"              /* filter_name */
+        "LocalTalk over UDP", /* name */
+        "LToUDP",			 /* short_name  */
+        "ltoudp"				 /* filter_name */
     );
 
     proto_register_field_array(proto_ltoudp, hf, array_length(hf));
@@ -58,8 +71,8 @@ void proto_register_ltoudp(void)
 void proto_reg_handoff_ltoudp(void)
 {
     static dissector_handle_t ltoudp_handle;
+
     ltoudp_handle = create_dissector_handle(dissect_ltoudp, proto_ltoudp);
-    
     llap_handle = find_dissector_add_dependency("llap", proto_ltoudp);
 
     dissector_add_uint("udp.port", LTOUDP_PORT, ltoudp_handle);
